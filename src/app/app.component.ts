38bfa40c790e9dba788f4557e99fdc817ed49a8e
avithemad <avithemad@gmail.com>
2021-01-16 14:57:50 +0530

    Added styling to navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
   export class AppComponent {
-    isOn: boolean = false;
+    isOn: boolean = true;
+    marg: number = 160;
     username: string = "avithemad";
     title: string = "THE BOOK STORE";
 
     toggleNavigation() {
       this.isOn = !this.isOn;
+      const mainbod = document.getElementById('main-bod');
+      if (mainbod)
+        mainbod.style.marginLeft=document.getElementById('main-bod')?.style.marginLeft=="0px"?"160px":"0px";
     }
 }
